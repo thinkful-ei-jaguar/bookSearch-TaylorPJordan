9bc8a8c1c6c66fd5a23e3370fdc5ce30c1850125
Jordan Castillo <jordanxcallaway@gmail.com>
2020-01-07 12:20:39 -0800

    book props displaying correctly

--- src/Book/Book.js
+++ src/Book/Book.js
@@ -3,13 +3,13 @@ import React, {Component} from 'react';
 class Book extends Component {
   render(){
     return(
-      <div className='book_item'>
-        <h2 className='book_title'>Henry I</h2>
-        <img href='#' alt='Cover of book' className='book_img'/>
+      <div className='book_item' id='{this.props.id}'>
+        <h2 className='book_title'>{this.props.title}</h2>
+        <img href='{this.props.image}' alt='Cover of book' className='book_img'/>
         <div className='book_info'>
-          <p className='author'>Author: C. Warren Hollister</p>
-          <p className='price'>Price: $50.00</p>
-          <p className='description'>Description</p>
+          <p className='author'>Author: {this.props.author}</p>
+          <p className='price'>Price:     {this.props.price}</p>
+          <p className='description'>{this.props.desc}</p>
         </div>
       </div>
     );
--- src/BookList/BookList.js
+++ src/BookList/BookList.js
@@ -2,14 +2,16 @@ import React, { Component } from 'react';
 import Book from '../Book/Book';
 
 class BookList extends Component {
-
+  
   render(){
     console.log(this.props);
+    const book = this.props.books.map((book) => 
+      <Book key={book.accessInfo.id} id={book.accessInfo.id} title={book.volumeInfo.title} image={book.volumeInfo.imageLink.smallThumbnail} author={book.volumeInfo.author} price={book.saleInfo.retailPrice.amount} desc={book.volumeInfo.description}/>
+    );
+
     return(
       <>
-      <Book />
-      <Book />
-      <Book />
+      {book}
       </>
     );
   }
